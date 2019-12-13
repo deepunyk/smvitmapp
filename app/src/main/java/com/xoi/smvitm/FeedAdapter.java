ba0e48a6525704347e354b9b30d5a6d518572f9d
ullaskunder <ullaskunder138@gmail.com>
2019-12-13 19:01:09 +0530

    Home Changed

--- app/src/main/java/com/xoi/smvitm/FeedAdapter.java
+++ app/src/main/java/com/xoi/smvitm/FeedAdapter.java
@@ -1,11 +1,14 @@
 package com.xoi.smvitm;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -23,11 +26,12 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
         this.feedList = feedList;
         this.context = context;
     }
+
     @NonNull
     @Override
     public FeedAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.feed_card,parent,false);
+                .inflate(R.layout.feed_card, parent, false);
 
         return new ViewHolder(v);
     }
@@ -39,12 +43,33 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
 
         holder.title.setText(items.getTitle());
         holder.description.setText(items.getDescription());
-        holder.imglink.setText(items.getImglink());
-        String imgurl=items.getImglink();
+        final String imgurl = items.getImglink();
+        final String title = items.getTitle();
+        final String description = items.getDescription();
 
         Glide.with(context)
                 .load(imgurl)
                 .into(holder.imgview);
+
+        holder.like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                try {
+                    Intent intent = new Intent(context, FeedDetails.class);
+                    intent.putExtra("title", title);
+                    intent.putExtra("description", description);
+                    intent.putExtra("imgurl", imgurl);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    }
+                    context.startActivity(intent);
+                }catch(Exception e){
+                    Toast.makeText(context, e.toString(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
 
     }
@@ -54,18 +79,22 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
         return feedList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView title,description,imglink;
-        public ImageView imgview;
+        public TextView title, description;
+        public ImageView imgview,like,comment,share;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            title = (TextView)itemView.findViewById(R.id.title);
-            description = (TextView)itemView.findViewById(R.id.description);
-            imglink = (TextView)itemView.findViewById(R.id.imglink);
-            imgview=(ImageView)itemView.findViewById(R.id.imgview);
+            title = (TextView) itemView.findViewById(R.id.title);
+            description = (TextView) itemView.findViewById(R.id.description);
+            imgview = (ImageView) itemView.findViewById(R.id.imgview);
+            like = (ImageView) itemView.findViewById(R.id.like);
+            comment = (ImageView) itemView.findViewById(R.id.comment);
+            share = (ImageView) itemView.findViewById(R.id.share);
+
+
         }
     }
 }
